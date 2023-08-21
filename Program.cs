d986da9cd2ccfdc93811de119d2184f51774925a
GustavoSousaBarbosa <gustavosousa3507@gmail.com>
2023-08-20 23:08:50 -0300

    criando um haikai,um poema curto

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.WriteLine("de tantos instantes");
+Console.WriteLine("para mim lembrança");
+Console.WriteLine("as flores de cerejeira.");
+Console.WriteLine("");
+Console.WriteLine("Matsuo Bashô");
